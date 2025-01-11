405c60ab6783f6395a6640da8aebd8764053c8ae
Kim So Yeon <qws1566@naver.com>
2025-01-11 18:00:57 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/org/example/skuhomepage/SkuHomepageApplication.java
@@ -0,0 +1,13 @@
+package org.example.skuhomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/skuhomepage/SkuHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skuhomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
