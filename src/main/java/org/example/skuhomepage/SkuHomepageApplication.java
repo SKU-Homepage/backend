eb9877dc4d23e4bfac807f422046ea40fbc8af4f
Kim So Yeon <qws1566@naver.com>
2025-02-07 16:40:15 +0900

    배포 파일 수정

--- src/main/java/org/example/skuhomepage/SkuHomepageApplication.java
+++ src/main/java/org/example/skuhomepage/SkuHomepageApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkuHomepageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkuHomepageApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SkuHomepageApplication.class, args);
+  }
 }
