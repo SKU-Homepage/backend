0774ee8bce1981276230c849a4e66a40a0035b6e
MeongW <easymiracle@skuniv.ac.kr>
2025-02-09 06:38:05 +0900

    ♻️ Refactor: Created 관련 응답 추가

--- src/main/java/org/example/skuhomepage/global/apiPayload/ApiResponse.java
+++ src/main/java/org/example/skuhomepage/global/apiPayload/ApiResponse.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,17 +16,30 @@ import lombok.Getter;
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
   @JsonProperty("isSuccess")
+  @Schema(description = "API 요청 성공 여부", example = "true")
   private final Boolean isSuccess;
 
+  @Schema(description = "API 응답 코드", example = "COMMON200")
   private final String code;
+
+  @Schema(description = "API 응답 메시지", example = "응답에 성공했습니다.")
   private final String message;
 
   @JsonInclude(JsonInclude.Include.NON_NULL)
+  @Schema(description = "API 응답 데이터")
   private T result;
 
   public static <T> ApiResponse<T> onSuccess(T result) {
     return new ApiResponse<>(
         true, CommonSuccessStatus._OK.getCode(), CommonSuccessStatus._OK.getMessage(), result);
+  }
+
+  public static <T> ApiResponse<T> onCreated(T location) {
+    return new ApiResponse<>(
+        true,
+        CommonSuccessStatus._CREATED.getCode(),
+        CommonSuccessStatus._CREATED.getReasonHttpStatus().getMessage(),
+        location);
   }
 
   public static <T> ApiResponse<T> of(BaseCode code, T result) {
--- src/main/java/org/example/skuhomepage/global/apiPayload/status/CommonSuccessStatus.java
+++ src/main/java/org/example/skuhomepage/global/apiPayload/status/CommonSuccessStatus.java
@@ -11,7 +11,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CommonSuccessStatus implements BaseCode {
   // Common Success
-  _OK(HttpStatus.OK, "COMMON200", "응답에 성공했습니다.");
+  _OK(HttpStatus.OK, "COMMON200", "응답에 성공했습니다."),
+  _CREATED(HttpStatus.CREATED, "COMMON201", "리소스가 성공적으로 생성되었습니다."),
+  ;
+
   private final HttpStatus httpStatus;
   private final String code;
   private final String message;
