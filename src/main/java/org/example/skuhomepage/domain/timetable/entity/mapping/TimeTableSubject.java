1362f71670d68959009dcca032b53db8c785bb03
Kim So Yeon <qws1566@naver.com>
2025-02-09 21:42:12 +0900

    :recycle: refactor: 과목, 시간표, 과목시간표 테이블로 엔티티 생성

---
+++ src/main/java/org/example/skuhomepage/domain/timetable/entity/mapping/TimeTableSubject.java
@@ -0,0 +1,7 @@
+package org.example.skuhomepage.domain.timetable.entity.mapping;
+
+import org.example.skuhomepage.global.common.BaseTimeEntity;
+
+public class TimeTableSubject extends BaseTimeEntity {
+
+}
