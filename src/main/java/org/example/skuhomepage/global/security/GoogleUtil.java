03a56b1d0675dbed3a6860a2ad895855bd617ea6
Kim So Yeon <qws1566@naver.com>
2025-02-28 13:36:36 +0900

    :sparkles: 리다이렉트 uri 추가

--- src/main/java/org/example/skuhomepage/global/security/GoogleUtil.java
+++ src/main/java/org/example/skuhomepage/global/security/GoogleUtil.java
@@ -29,6 +29,9 @@ public class GoogleUtil {
   @Value("${google.deploy-redirect-uri}") // 배포용 리디렉트 URI
   private String deployRedirectUri;
 
+  @Value("${google.dev-redirect-uri}")
+  private String devRedirectUri; // 개발 서버 리디렉트 URI
+
   private static final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
   private static final String GOOGLE_USER_INFO_URL =
       "https://www.googleapis.com/oauth2/v3/userinfo";
@@ -40,8 +43,10 @@ public class GoogleUtil {
       return localRedirectUri;
     } else if (env == 1) {
       return deployRedirectUri;
+    } else if (env == 2) {
+      return devRedirectUri;
     } else {
-      throw new IllegalArgumentException("잘못된 환경 값입니다. (0: 로컬, 1: 배포)");
+      throw new IllegalArgumentException("잘못된 환경 값입니다. (0: 로컬, 1: 배포, 2: 개발)");
     }
   }
 
@@ -66,11 +71,10 @@ public class GoogleUtil {
 
     HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
 
-    
     ResponseEntity<String> response =
         restTemplate.exchange(GOOGLE_TOKEN_URL, HttpMethod.POST, requestEntity, String.class);
     log.info(response.getBody());
-    
+
     try {
 
       log.info("구글 토큰 값: {}", response.getBody());
