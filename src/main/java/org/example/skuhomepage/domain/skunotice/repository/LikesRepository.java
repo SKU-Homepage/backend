602623d0022c918f1f7bdaab64a444d3c58b8fdc
MeongW <easymiracle@skuniv.ac.kr>
2025-03-11 09:20:11 +0900

    :sparkles: Feature: 찜 기능 추가

--- src/main/java/org/example/skuhomepage/domain/skunotice/repository/LikesRepository.java
+++ src/main/java/org/example/skuhomepage/domain/skunotice/repository/LikesRepository.java
@@ -1,8 +1,8 @@
 package org.example.skuhomepage.domain.skunotice.repository;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.example.skuhomepage.domain.mypage.entity.User;
 import org.example.skuhomepage.domain.skunotice.entity.Likes;
 import org.example.skuhomepage.domain.skunotice.entity.SkuNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,10 +11,18 @@ import org.springframework.data.jpa.repository.Query;
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
   @Query(
-      "SELECT l.skuNotice FROM Likes l WHERE l.user = :user "
+      "SELECT l.skuNotice FROM Likes l WHERE l.user.Id = :userId "
           + "AND l.skuNotice.author = '교수학습원' AND l.skuNotice.author = '진로취업지원센터' "
           + "AND l.skuNotice.author = '대학혁신지원사업단'")
-  List<SkuNotice> findECNoticeLikesByUser(User user);
+  List<SkuNotice> findECNoticeLikesByUser(Long userId);
+
+  @Query(
+      "SELECT l.skuNotice FROM Likes l WHERE l.user.Id = :userId "
+          + "AND l.skuNotice.author != '교수학습원' AND l.skuNotice.author != '진로취업지원센터' "
+          + "AND l.skuNotice.author != '대학혁신지원사업단'")
+  List<SkuNotice> findNoticeLikesByUser(Long userId);
 
   boolean existsByUserIdAndSkuNotice(Long userId, SkuNotice skuNotice);
+
+  Optional<Likes> findByUserIdAndSkuNoticeId(Long userId, Long noticeId);
 }
