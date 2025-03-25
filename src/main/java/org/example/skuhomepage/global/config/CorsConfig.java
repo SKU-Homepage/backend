21d509294b93c713af5386633d83bf7ec0324548
MeongW <easymiracle@skuniv.ac.kr>
2025-03-25 14:25:43 +0900

    :wrench: Setting: cors allowedOrigins 설정파일로 분리

--- src/main/java/org/example/skuhomepage/global/config/CorsConfig.java
+++ src/main/java/org/example/skuhomepage/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.example.skuhomepage.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,16 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+  @Value("${cors.allowed-origins")
+  private String[] allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins(
-            "http://localhost:3000",
-            "https://devsku.netlify.app",
-            "https://www.skuniv.co.kr",
-            "http://localhost:8080",
-            "https://api.skuniv.co.kr")
+        .allowedOrigins(allowedOrigins)
         .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
